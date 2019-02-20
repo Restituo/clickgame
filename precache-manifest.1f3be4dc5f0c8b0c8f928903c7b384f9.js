f8f22c3157ed9d76d0a84da81d943c1e3fc32106
Restituo <rcc999@comcast.net>
2019-02-20 14:00:39 -0800

    Updates

---
+++ precache-manifest.1f3be4dc5f0c8b0c8f928903c7b384f9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1069ab6f31668351a94985d206797f6f",
+    "url": "/clickgame/static/media/clownfish.1069ab6f.jpg"
+  },
+  {
+    "revision": "a02727a00c862dd903f4",
+    "url": "/clickgame/static/css/main.8ec998c3.chunk.css"
+  },
+  {
+    "revision": "0f98500e31e84fb21f0a",
+    "url": "/clickgame/static/js/runtime~main.0f98500e.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/clickgame/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "5d65332cf7c3974591a8283a3f7b3bc8",
+    "url": "/clickgame/static/media/angelfish.5d65332c.jpg"
+  },
+  {
+    "revision": "52f65e451636729b18a6761c0bc172dd",
+    "url": "/clickgame/static/media/barracuda.52f65e45.jpeg"
+  },
+  {
+    "revision": "321b172f690c954a5d057d9cb19abb40",
+    "url": "/clickgame/static/media/butterfly.321b172f.jpg"
+  },
+  {
+    "revision": "df16d5fde20aefd05a35dee5e782b6e7",
+    "url": "/clickgame/static/media/chromis.df16d5fd.jpg"
+  },
+  {
+    "revision": "a02727a00c862dd903f4",
+    "url": "/clickgame/static/js/main.a02727a0.chunk.js"
+  },
+  {
+    "revision": "49b9dc5fc0dbf35c2a125d2532adaa4e",
+    "url": "/clickgame/static/media/creolewrasse.49b9dc5f.jpg"
+  },
+  {
+    "revision": "228f9d1463cbbe3b5e0b57908d7ec93c",
+    "url": "/clickgame/static/media/damselfish.228f9d14.jpg"
+  },
+  {
+    "revision": "06b9c3169370894762d49745d40de4eb",
+    "url": "/clickgame/static/media/grunt.06b9c316.jpg"
+  },
+  {
+    "revision": "b27ddfcfde7efbf2c4d67b3badcfda94",
+    "url": "/clickgame/static/media/parrotfish.b27ddfcf.jpg"
+  },
+  {
+    "revision": "b376e509de7469b2f0d252f40a1875cb",
+    "url": "/clickgame/static/media/rockbeauty.b376e509.jpg"
+  },
+  {
+    "revision": "4395feb1de54b8ed0e358013f32c9b1f",
+    "url": "/clickgame/static/media/sergeantmajor.4395feb1.JPG"
+  },
+  {
+    "revision": "5247794b72eedcac58a115add1c81d9a",
+    "url": "/clickgame/static/media/tang.5247794b.jpg"
+  },
+  {
+    "revision": "f455f159df58c88f37683d52f223f920",
+    "url": "/clickgame/index.html"
+  }
+];
